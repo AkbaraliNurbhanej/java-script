e45df81ec53b7294adbf87e6fba665417f0d42c1
AkbaraliNurbhanej <akbaralinurbhanej99@gmail.com>
2024-09-20 11:56:35 +0530

    add.

--- app.js
+++ app.js
@@ -219,3 +219,14 @@ function yell(msg){
     
 }
 }
+
+
+// forEach !!!!!
+
+const number = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+number.forEach(function (el){
+    if(el % 2 === 0){
+    console.log(el)
+    }
+})
